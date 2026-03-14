29ef7872ee17364c0161534d4970c6c9b720a90c
Ahmed Mohamed Attia <108270244+A-M-Attia@users.noreply.github.com>
2026-03-14 11:53:34 +0200

    feat: render Top Brands data in Best Sellers section

--- src/components/GenInfo.jsx
+++ src/components/GenInfo.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import BrandsProductCard from "./BrandsProductCard";
 
 const GenInfo = () => {
   const data = [
@@ -58,26 +59,7 @@ export const Brands = ({ data }) => {
       <p className="child:ring-white font-bold mb-6">Top Brands</p>
       <div className="flex flex-wrap justify-center">
         {data.map((elem, id) => (
-          <div
-            key={id}
-            className="relative w-[340px] h-[340px] mx-2 mb-6 hover:text-white"
-          >
-            <div className="absolute w-full flex justify-center items-center top-4  ">
-              <p className="logo font-semibold z-50 ">{elem.name}</p>
-            </div>
-            <img
-              src={elem.src}
-              alt={elem.name}
-              className="w-full h-full object-cover"
-            />
-            <button
-              onClick={() => navigate(elem.to)}
-              className="absolute inset-0 flex items-center justify-center
-                             bg-gray-800 text-white opacity-0 hover:opacity-80 transition-opacity duration-200"
-            >
-              Explore →
-            </button>
-          </div>
+          <BrandsProductCard elem={elem} key={id} />
         ))}
       </div>
     </div>
